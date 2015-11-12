c3fb3c75c20516b8e0b35e5df12f9e6702234d00
rluick15 <rluick15@gmail.com>
2015-11-11 20:27:05 -0500

    updated

--- app/src/main/java/com/richluick/beacontest/BeaconListAdapter.java
+++ app/src/main/java/com/richluick/beacontest/BeaconListAdapter.java
@@ -6,50 +6,43 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import org.altbeacon.beacon.Beacon;
+
 import java.util.ArrayList;
 
 /**
  * Created by q1410049 on 11/9/15.
  */
-public class BeaconListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class BeaconListAdapter extends RecyclerView.Adapter<BeaconListAdapter.BeaconViewHolder> {
 
-    private ArrayList<eBeacon> mBeacons;
+    private ArrayList<Beacon> mBeacons;
 
-    public BeaconListAdapter(ArrayList<eBeacon> beaconList) {
+    public BeaconListAdapter(ArrayList<Beacon> beaconList) {
         this.mBeacons = beaconList;
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public BeaconListAdapter.BeaconViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.beacon_list_adapter, parent, false);
         return new BeaconViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        BeaconViewHolder beaconViewHolder = (BeaconViewHolder) holder;
+    public void onBindViewHolder(BeaconListAdapter.BeaconViewHolder holder, int position) {
+        String dist = String.format("%.2f", mBeacons.get(position).getDistance());
 
-        beaconViewHolder.beaconName.setText(mBeacons.get(position).getBeaconName());
-        beaconViewHolder.beaconDist.setText(mBeacons.get(position).getBeaconDistance());
+        holder.beaconName.setText(mBeacons.get(position).getBluetoothName());
+        holder.beaconDist.setText(dist + " m");
+    }
+
+    public void updateBeaconList(ArrayList<Beacon> beacons) {
+        mBeacons = beacons;
+        notifyDataSetChanged();
     }
 
     @Override
     public int getItemCount() {
         return mBeacons.size();
-    }
-
-    public void add(Beacon beacon) {
-        if(!mBeacons.contains(beacon)) {
-            mBeacons.add(beacon);
-            notifyDataSetChanged();
-        }
-    }
-
-    public void remove(Beacon beacon) {
-        if(mBeacons.contains(beacon)) {
-            mBeacons.remove(beacon);
-            notifyDataSetChanged();
-        }
     }
 
     public class BeaconViewHolder extends RecyclerView.ViewHolder {
--- app/src/main/java/com/richluick/beacontest/MainActivity.java
+++ app/src/main/java/com/richluick/beacontest/MainActivity.java
@@ -1,76 +1,39 @@
 package com.richluick.beacontest;
 
+import android.os.Bundle;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.widget.TextView;
-import android.util.Log;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
-import org.altbeacon.beacon.Identifier;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.nearby.Nearby;
-import com.google.android.gms.nearby.messages.Message;
-import com.google.android.gms.nearby.messages.MessageListener;
-import com.google.android.gms.nearby.messages.Strategy;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
 public class MainActivity extends AppCompatActivity implements BeaconConsumer, RangeNotifier {
 
-    private GoogleApiClient mGoogleApiClient;
     private ArrayList<Beacon> mBeacons = new ArrayList<>();
     private BeaconListAdapter mAdapter;
-
-    MessageListener mMessageListener = new MessageListener() {
-        // Called each time a new message is discovered nearby.
-        @Override
-        public void onFound(Message message) {
-            Log.i("BeaconFound", "Found message: " + message);
-
-            Beacon beacon = new Beacon();
-            beacon.setBeaconName(message.getNamespace());
-            mAdapter.add(beacon);
-        }
-
-        // Called when a message is no longer nearby.
-        @Override
-        public void onLost(Message message) {
-            Log.i("BeaconLost", "Lost message: " + message);
-
-            Beacon beacon = new Beacon();
-            beacon.setBeaconName(message.getNamespace());
-            mAdapter.remove(beacon);
-        }
-    };;
 
     private BeaconManager mBeaconManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //todo: check for activated bluetooth, as well as permission
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //populate this list with beacons
-        ArrayList<eBeacon> beacons = new ArrayList<>();
-
-        BeaconListAdapter adapter = new BeaconListAdapter(beacons);
         mAdapter = new BeaconListAdapter(mBeacons);
         RecyclerView beaconList = (RecyclerView) findViewById(R.id.beaconList);
-        beaconList.setAdapter(adapter);
+        beaconList.setLayoutManager(new LinearLayoutManager(this));
+        beaconList.setAdapter(mAdapter);
     }
 
     @Override
@@ -98,7 +61,7 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer, R
 
     @Override
     public void onBeaconServiceConnect() {
-        Region region = new Region("all-beacons-region", null, null, null);
+        Region region = new Region("ibeacon-region", null, null, null);
         try {
             mBeaconManager.startRangingBeaconsInRegion(region);
         } catch (RemoteException e) {
@@ -108,67 +71,13 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer, R
     }
 
     @Override
-    public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-        for (Beacon beacon: beacons) {
-            //if (beacon.getServiceUuid() == 0xfeaa && beacon.getBeaconTypeCode() == 0x00) {
-            // This is a Eddystone-UID frame
-            Identifier namespaceId = beacon.getId1();
-            Identifier instanceId = beacon.getId2();
-            Log.d("RangingActivity", "I see a beacon transmitting namespace id: " + namespaceId +
-                    " and instance id: " + instanceId +
-                    " approximately " + beacon.getBluetoothName() + " meters away.");
-            runOnUiThread(new Runnable() {
-                public void run() {
-                    ((TextView) MainActivity.this.findViewById(R.id.message)).setText("Hello world, and welcome to Eddystone!");
-                }
-            });
-            //}
-        beaconList.setAdapter(mAdapter);
-    }
+    public void didRangeBeaconsInRegion(final Collection<Beacon> beacons, Region region) {
+        //todo: might be good to create custom beacon class and use a list of that
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Nearby.MESSAGES_API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    protected void onStop() {
-        if (mGoogleApiClient.isConnected()) {
-            Nearby.Messages.unsubscribe(mGoogleApiClient, mMessageListener);
-            mGoogleApiClient.disconnect();
-        }
-        super.onStop();
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        if (!mGoogleApiClient.isConnected() && !mGoogleApiClient.isConnecting()) {
-            mGoogleApiClient.connect();
-        } else {
-            Nearby.Messages.subscribe(mGoogleApiClient, mMessageListener, Strategy.BLE_ONLY)
-                    .setResultCallback(new ResultCallback<Status>() {
-                        @Override
-                        public void onResult(Status status) {
-
-                        }
-                    });
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-
+        runOnUiThread(new Runnable() {
+            public void run() {
+                mAdapter.updateBeaconList((ArrayList<Beacon>) beacons);
+            }
+        });
     }
 }
